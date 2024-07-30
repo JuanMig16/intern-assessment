a5fb4a4e0774e747dceff30c0a77d31dc91857e9
Darlo Miguel Ilagan <d.ilagan@sparkles.com.ph>
2024-07-30 14:15:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/api/helloworld", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.post("/api/name", (req, res) => {
+  const { firstName, lastName } = req.body;
+  res.json({ message: `Hello ${firstName} ${lastName}` });
+});
+
+app.post("/api/add", (req, res) => {
+  const { num1, num2 } = req.body;
+  res.json({ message: num1 + num2 });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
