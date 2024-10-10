f1f09f9fb257a278d6e29e06a71aee0879d081b8
JuanMig16 <sanchez.juanmiguel16@gmail.com>
2024-10-10 08:27:14 +0800

    complete version

--- index.js
+++ index.js
@@ -10,18 +10,21 @@ app.get("/api/helloworld", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+// Task 1 and 2
 app.post("/api/name", (req, res) => {
   const { firstName, lastName } = req.body;
   console.log(firstName)
   res.json({ message: `Hello ${firstName} ${lastName}` });
 });
 
+// Task 3
 app.post("/api/add", (req, res) => {
   const { num1, num2 } = req.body;
   const sum = num1 + num2;
   res.json({ message: num1 + num2 });
 });
 
+//Task 4 and 5
 app.post("/api/sub", (req, res) => {
   const { number1, number2 } = req.body;
   const result = number1 - number2;
